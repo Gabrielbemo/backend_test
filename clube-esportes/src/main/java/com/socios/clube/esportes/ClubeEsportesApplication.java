43e28168738be3c63bdcdca4219500f14bee73c0
Gabrielbemo <gabrielbemoura@gmail.com>
2021-10-18 13:57:20 -0300

    Estrutura inicial

---
+++ clube-esportes/src/main/java/com/socios/clube/esportes/ClubeEsportesApplication.java
@@ -0,0 +1,13 @@
+package com.socios.clube.esportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubeEsportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubeEsportesApplication.class, args);
+	}
+
+}
---
+++ clube-esportes/src/test/java/com/socios/clube/esportes/ClubeEsportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socios.clube.esportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubeEsportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

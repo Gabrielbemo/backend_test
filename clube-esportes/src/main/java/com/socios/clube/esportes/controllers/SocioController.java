1a810cca56980e6fa46b9f8ffd3a2ea7f249d236
Gabrielbemo <gabrielbemoura@gmail.com>
2021-10-22 20:55:43 -0300

    adicionado path list socio e testes unitarios

--- clube-esportes/src/main/java/com/socios/clube/esportes/controllers/SocioController.java
+++ clube-esportes/src/main/java/com/socios/clube/esportes/controllers/SocioController.java
@@ -2,6 +2,7 @@ package com.socios.clube.esportes.controllers;
 
 import com.socios.clube.esportes.controllers.dtos.in.CreateSocioRequestDTO;
 import com.socios.clube.esportes.controllers.dtos.out.CreateSocioResponseDTO;
+import com.socios.clube.esportes.controllers.dtos.out.ListSocioResponseDTO;
 import com.socios.clube.esportes.models.Socio;
 import com.socios.clube.esportes.services.SocioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/socios")
@@ -21,6 +24,20 @@ public class SocioController {
     @Autowired
     public SocioController(final SocioService socioService){
         this.socioService = socioService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ListSocioResponseDTO> getById(@PathVariable final long id){
+        return new ResponseEntity<ListSocioResponseDTO>(
+                ListSocioResponseDTO.fromEntity(socioService.getById(id)),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ListSocioResponseDTO>> list(){
+        return new ResponseEntity<List<ListSocioResponseDTO>>(
+                socioService.list().stream().map(socio -> ListSocioResponseDTO.fromEntity(socio)).collect(Collectors.toList()),
+                HttpStatus.OK);
     }
 
     @PostMapping("/")
